7fce8ba0f4935b400f26c1e2e900010c2a975875
Prinolan Govender <prinolan009@gmail.com>
2018-09-13 20:21:03 +0200

    Finalized

--- src/SortingAlgorithms/SortByName.java
+++ src/SortingAlgorithms/SortByName.java
@@ -8,24 +8,41 @@ import java.util.Scanner;
 import Worker.Person;
 
 public class SortByName {
-//aaa
+private int choice;
+private int num;
+private int count;
+private String name;
+
+	public void MoreThanOneWorker(){
+		Scanner kb=new Scanner(System.in);
+		while(this.choice==0 || this.choice>2){
+			if(this.count>1){
+				System.out.println("More than 1 employee exists with that name");
+				System.out.println("1) Display all workers with this name");
+				System.out.println("2) Search by Employer ID");
+				this.choice=kb.nextInt();
+				
+				if(this.choice==2){
+					System.out.println("Enter worker ID");
+					this.num=kb.nextInt();
+				}
+				//num=kb.nextInt();
+				if(this.choice>3){
+					System.out.println("Invalid entry");//makes user pick again if incorrect entry
+				}
+			}
+		}
+	}//ends option list
+	
+	
 	public void FindPerson(String name) throws ParseException{
-		//question 1 from assessment
+	
 		boolean found=false;
-		//name=name.toLowerCase();
-		
 		Person per=new Person();
 		per.read();
-		Scanner kb=new Scanner(System.in);
-		int counter=0;
-		int choice=0;
 		boolean flag=false;
-		/*for(int i=0;i<temp.size();i++){
-			System.out.println(temp.get(i));
-		}*/
-		int count=0;
 		String temp="";
-		int num=0;
+		
 		//checks if more than 1 employee exists with the same name
 		for(int i=0;i<per.getName().size();i++){
 			temp=per.getName().get(i).trim();
@@ -35,31 +52,18 @@ public class SortByName {
 				count++;
 			}
 		}
-		
-		System.out.println(count);
-		while(choice==0 || choice>2){
-			if(count>1){
-				System.out.println("More than 1 employee exists with that name");
-				System.out.println("1) Display all workers with the name: "+name);
-				System.out.println("2) Search by Employer ID");
-				choice=kb.nextInt();
-				
-				if(choice==2){
-					System.out.println("Enter worker ID");
-					num=kb.nextInt();
-				}
-				//num=kb.nextInt();
-				if(choice>3){
-					System.out.println("Invalid entry");
-				}
-			}
+		if(count>1){
+			this.MoreThanOneWorker();//envokes method if more than one worker with the specified name
 		}
+		
+		
+	
 		SimpleDateFormat format=new SimpleDateFormat("dd-MM-yyyy");
 		
 		
 		
 		for(int i=0;i<per.getName().size();i++){
-			temp=per.getName().get(i).trim();//removes spacing
+			temp=per.getName().get(i);//removes spacing
 			temp=temp.toLowerCase();//converts to lower case
 			name=name.toLowerCase();//converts to lower case
 			
@@ -67,14 +71,8 @@ public class SortByName {
 			
 			if(temp.equals(name) ){//only one worker with the name
 				found=true;
-				System.out.println("\nName:\t\t\t"+per.getName().get(i));
-				System.out.println("Surname:\t\t"+per.getSurname().get(i));
-				System.out.println("DOB:\t\t\t"+format.format(per.getDOB().get(i)));
-				System.out.println("Employee Num:\t\t"+per.getEmpNum().get(i));
-				System.out.println("Salary:\t\t\t"+per.getSalary().get(i));
-				System.out.println("Designation:\t\t"+per.getDesignate().get(i));
-				System.out.println("Reports to:\t\t"+per.getReport().get(i));
-				System.out.println("----------------------------------------------------------");
+				System.out.println(this.constructString(per,i));
+				
 			}
 			}//ends choice ==1
 			//checks if more than 1 worker has the same name
@@ -83,19 +81,12 @@ public class SortByName {
 				
 				
 				for(int j=0;j<per.getEmpNum().size();j++){
-					//System.out.println("num"+num);
+					
 					int id=per.getEmpNum().get(j);
-				//	System.out.println("emp"+id);
+				
 					if(id==num){
-						System.out.println("Name:\t\t\t"+per.getName().get(j));
-						System.out.println("Surname:\t\t"+per.getSurname().get(j));
-						System.out.println("DOB:\t\t\t"+format.format(per.getDOB().get(j)));
-						System.out.println("Employee Num:\t\t"+per.getEmpNum().get(j));
-						System.out.println("Salary:\t\t\t"+per.getSalary().get(j));
-						System.out.println("Designation:\t\t"+per.getDesignate().get(j));
-						System.out.println("Reports to:\t\t"+per.getReport().get(j));
-						//flag=true;
-						break;
+						System.out.println(this.constructString(per, j));
+						break;//terminates when worker is found
 					}
 					if(flag==false){
 						System.out.println("No employee found with the ID: "+num);
@@ -110,22 +101,15 @@ public class SortByName {
 			
 		}//ends for loop I
 		
-	}//ends FindPerson
-	// per.getDOB().forEach(action-> System.out.println("DOB:\t\t"+format.format(action)));
-	/*Scanner kb=new Scanner(System.in);//used to take user input
+	}//ends find person method
 	
-	int num=0;
-	int count=0;//used to check the amount of workers with the same name
-	boolean found=false;
-	Date date=new Date();*/
-	//traverse through list to find name, if names are equal, return data relating to person
+	public String constructString(Person p,int i){
+		SimpleDateFormat sdf = new SimpleDateFormat("dd-MM-yyyy");
+		return "\nName:\t\t\t"+p.getName().get(i)+"\nSurname:\t\t"+p.getSurname().get(i)+
+				"\nDOB:\t\t\t"+sdf.format(p.getDOB().get(i))+"\nEmployee Num:\t\t"+p.getEmpNum().get(i)
+				+"\nSalary:\t\t\t"+p.getSalary().get(i)+"\nDesignation:\t\t"+p.getDesignate().get(i)
+				+"\nReports to:\t\t"+p.getReport().get(i)+	"\n---------------------------------------------------";
+	}
 	
-/*	for(int i=0;i<per.getName().size();i++){
-		//checks if more than 1 person exists with the same name
-		if(per.getName().get(i).equals(name)){
-			
-			count++;
-		}
-		
-	}*/
+	
 }
