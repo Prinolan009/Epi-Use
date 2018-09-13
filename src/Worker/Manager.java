e154498c1cb31c8164b833a316ce8f84d2d825ac
Prinolan Govender <prinolan009@gmail.com>
2018-09-13 18:31:51 +0200

    Added Subordinate count

--- src/Worker/Manager.java
+++ src/Worker/Manager.java
@@ -7,12 +7,20 @@ public class Manager extends Person{
 	//the following class is open for extension for the future
 	
 	private int NumberOfManagers;
-
+	private int NumberOfSubordinates;
 	public Manager() throws ParseException{this.read();}
 
 	
+	public void setNumberOfSubordinates(){
+		for(int i=0;i<this.getDesignate().size();i++){
+			if(this.getReport().get(i).trim().equals("Manager")){
+				this.NumberOfSubordinates++;
+			}
+		}
+	}
+	
 	public void setNumberOfManagers(){
-		
+		//sets the number of managers within the company
 		for(int i=0;i<this.getDesignate().size();i++){
 			if(this.getDesignate().get(i).trim().equals("Manager")){
 				this.NumberOfManagers++;
@@ -21,6 +29,9 @@ public class Manager extends Person{
 	}
 	
 	public int getNumberOfManagers(){
-	return 	this.NumberOfManagers;
+		return 	this.NumberOfManagers;
 	} 
+	public int getNumberOfSubordinates(){
+		return 	this.NumberOfSubordinates;
+		} 
 }
